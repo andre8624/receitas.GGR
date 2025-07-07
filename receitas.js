192fe91ba27a33446618d55637cd5534a8bd9717
andre8624 <161709690+andre8624@users.noreply.github.com>
2025-07-07 08:19:24 -0300

    s

---
+++ receitas.js
@@ -0,0 +1,23 @@
+criaCartao('', '')
+
+criaCartao('', 'Como se calcula a energia cinetica de um objeto?','Usamos a fórmula Ec=mv²/2')
+
+criaCartao('', 'Qual era a justificativa que Vargas deu para dar um golpe de estado?','Ele utilizou como pretesto, o plano cohen, que era um suposto plano comunista para tomar o poder')
+
+criaCartao('', 'Como calcular a velocidade média de consumo?','V=Δn/Δt')
+
+criaCartao('Física', 'Como calcular a velociadade média?',' usamos a fórmula, Vm=Δns/Δt')
+
+criaCartao('Física', 'Qual é a formula do Movimento Uniforme(MU)?','S=S0+V∆t')
+
+criaCartao('', 'Como se calcula a Energia de Ativação(EAT)?','utiliza-se a fórmula EAT = ECA - HR')
+
+criaCartao('Matemática','Qual é a fórmula da função afim?','utiliza-se a fórmula f(x) ax+b')
+
+criaCartao('Matemática','Qual é a fórmula usada para calcular o seno?','utiliza-se a fórmula CO/H')
+
+criaCartao('Matemática','Qual é a fórmula usada para calcular o cosseno?','utiliza-se a fórmula CA/H')
+
+criaCartao('Matemática','Qual é a fórmula usada para calcular a Tangente?','utiliza-se a fórmula CO/CA')
+
+criaCartao('a','O que foi a guerra do contestado?','foi um conflito armado ocorrido de outubro de 1912 a agosto de 1916, que teve como partes beligerantes posseiros e pequenos proprietários de terras contra os governos dos estados de Santa Catarina e Paraná, além do Governo Federal brasileiro')
