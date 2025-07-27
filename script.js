ca160b6a2a104ed7367eae128e26066208ad16b9
andre8624 <161709690+andre8624@users.noreply.github.com>
2025-07-27 15:54:53 -0300

    s

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ d
   
 }
 function mostraReposta (card)   {
-    const resposta = card.querySelector ('.resposta-cartao', '.resposta2-cartao')
+    const resposta = card.querySelector ('.resposta-cartao')
 if(resposta.style.display === 'none'){
 resposta.style.display = 'block';
 
