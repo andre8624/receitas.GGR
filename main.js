e24a46f8279196bcd9ebb71da0c91b3decbc2d12
andre8624 <161709690+andre8624@users.noreply.github.com>
2025-07-27 00:12:58 -0300

    s

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class = "conteudo-cartao" onclick = "mostraReposta(this)">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
@@ -15,12 +15,20 @@ function criaCartao (categoria, pergunta, resposta){
     `
 
  container.appendChild(cartao);
- let respostaEstaVisivel
-    function viraCartao(){
-        respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active',respostaEstaVisivel);
-    }
-cartao.addEventListener('click',viraCartao)
-    container.appendChild(cartao);
+
   
+}
+function mostraReposta (card)   {
+    const resposta = card.querySelector ('.resposta-cartao')
+if(resposta.style.display === 'none'){
+resposta.style.display = 'block';
+
+
+
+}
+    else{
+        resposta.style.display = 'none';
+
+
+    }
 }
