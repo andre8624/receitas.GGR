c8e7c032d2c7cab92f0e519f1c1849ef1f1e83da
andre8624 <161709690+andre8624@users.noreply.github.com>
2025-07-29 16:03:15 -0300

    s

--- receitas.js
+++ receitas.js
@@ -1,4 +1,4 @@
-criaCartao('classe' ,'pergunta ou o que tu quiser falar','<div class="parent"> div class="div1"> <img class="ilustrativo" src="image copy.png" alt="ilustrativo"> </div> <div class="div2">a alalallalalalalallalalalla</div> <div class="div3"> </div> </div>' )
+criaCartao('classe' ,'pergunta ou o que tu quiser falar','<div class="parent"> <div class="div1"> <img class="ilustrativo" src="image copy.png" alt="ilustrativo"> </div> <div class="div2">a alalallalalalalallalalalla</div> <div class="div3"> </div> </div>' )
 
 criaCartao('classe' ,'pergunta ou o que tu quiser falar','<ul><li>sssssssssssssssss</li>sssss</ul>ssssssssssssssssssssssssssssssssssssss' )
 
