b138cae11bd0db2d6eebf074d1b0c3d68eaaac26
andre8624 <161709690+andre8624@users.noreply.github.com>
2025-07-29 17:24:18 -0300

    d

--- receitas.js
+++ receitas.js
@@ -1,8 +1,4 @@
-criaCartao('classe' ,'pergunta ou o que tu quiser falar','<img class="ilustrativo" src="image copy.png" alt="ilustrativo"> <ul class"preparo"><li>Lorem ipsum dolor sit amet. Nam neque dolorum eum quidem commodi et ullam debitis. Vel culpa deleniti qui quidem dolor sit neque voluptatibus aut iusto cupiditate. Qui fugit magni ab voluptates voluptatem est blanditiis similique qui omnis adipisci qui iure doloremque?
-
-Est aliquam animi ut sunt nihil aut delectus quae et fuga impedit aut nihil quos sed culpa itaque sed ullam eaque. Sit labore laboriosam et porro corporis ut temporibus sequi ad iure dolorum aut perspiciatis eius est harum sunt est galisum veniam. Eum dolor excepturi ut aliquid quibusdam nam placeat asperiores sed quia accusantium.
-
-Et aliquam accusamus nam internos optio ut asperiores animi non sunt labore et deleniti atque ut quod dolorem. Aut nobis autem et distinctio quia est quidem internos ut nihil soluta et quae itaque ad unde quaerat et labore quos?</li>  <li>2</li>  <li>3</li> <li>4</li></ul> <ul class "ingredientes"><li>1</li></ul>' )
+criaCartao('classe' ,'pergunta ou o que tu quiser falar','<img class="ilustrativo" src="image copy.png" alt="ilustrativo"> <ul class"preparo"><li>Lorem ipsum dolor sit amet. Nam neque dolorum eum quidem commodi et ullam debitis. Vel culpa deleniti qui quidem dolor sit neque voluptatibus aut iusto cupiditate. Qui fugit magni ab voluptates voluptat</li>  <li>2</li>  <li>3</li> <li>4</li></ul> <ul class "ingredientes"><li>1</li></ul>' )
 
 criaCartao('classe' ,'pergunta ou o que tu quiser falar','<ul><li>sssssssssssssssss</li>sssss</ul>ssssssssssssssssssssssssssssssssssssss' )
 
