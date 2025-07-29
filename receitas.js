951c0c9618c0d305a6d7c9397ff14b776d1ce885
andre8624 <161709690+andre8624@users.noreply.github.com>
2025-07-29 16:39:48 -0300

    s

--- receitas.js
+++ receitas.js
@@ -1,4 +1,4 @@
-criaCartao('classe' ,'pergunta ou o que tu quiser falar','<img class="ilustrativo" src="image copy.png" alt="ilustrativo"> <p class"preparo">a alalallalalalalallalalalla</p> <p class "ingredientes">a</p> <br>' )
+criaCartao('classe' ,'pergunta ou o que tu quiser falar','<img class="ilustrativo" src="image copy.png" alt="ilustrativo"> <p class"preparo">a alalallalalalalallalalalla</p> <p class "ingredientes">a</p> ' )
 
 criaCartao('classe' ,'pergunta ou o que tu quiser falar','<ul><li>sssssssssssssssss</li>sssss</ul>ssssssssssssssssssssssssssssssssssssss' )
 
