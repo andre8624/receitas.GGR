060e75e3a75970c458301a46e9e9f25d620c5d4a
andre8624 <161709690+andre8624@users.noreply.github.com>
2025-08-17 23:54:49 -0300

    p

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
-function criaCartao (categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta, imagemSrc, imagemAlt) {
     let container = document.getElementById('container');
-    let cartao = document.createElement('article');
+    let cartao = document.createElement('div');
     cartao.className = 'cartao';
+
     cartao.innerHTML = `
-    <div class = "conteudo-cartao" onclick = "mostraReposta(this)">
-    <h1>${categoria}</h1>
-    <div class="pergunta-cartao">
-        <h4>${pergunta}</h4>
-    </div>
-    <div class="resposta-cartao">
-        <p>${resposta}</p>
-    </div>
-    
-</div>
-    `
+        <img src="${imagemSrc}" alt="${imagemAlt}" class="cartao-imagem">
+        <div class="conteudo-texto">
+            <div class="conteudo-cartao" onclick="mostraResposta(this)">
+                <h1>${categoria}</h1>
+                ${pergunta ? `<h4>${pergunta}</h4>` : ''}
+                <div class="resposta-cartao">
+                    <p>${resposta}</p>
+                </div>
+            </div>
+        </div>
+    `;
 
- container.appendChild(cartao);
-
-  
-}
-function mostraReposta (card)   {
-    const resposta = card.querySelector ('.resposta-cartao')
-if(resposta.style.display === 'none'){
-resposta.style.display = 'block';
-
-
-
-}
-    else{
-        resposta.style.display = 'none';
-
-
-    }
+    container.appendChild(cartao);
 }
