50daacce335187628b2fb33b79a61f925dd514a4
andre8624 <161709690+andre8624@users.noreply.github.com>
2025-08-17 23:57:35 -0300

    p

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-function criaCartao(categoria, pergunta, resposta, imagemSrc, imagemAlt) {
+function criaCartao (categoria, pergunta, resposta){
     let container = document.getElementById('container');
-    let cartao = document.createElement('div');
+    let cartao = document.createElement('article');
     cartao.className = 'cartao';
-
     cartao.innerHTML = `
-        <img src="${imagemSrc}" alt="${imagemAlt}" class="cartao-imagem">
-        <div class="conteudo-texto">
-           <button class="botao-expansao" onclick="mostraResposta(this)">Mostrar Receita</button>
-                <h1>${categoria}</h1>
-                ${pergunta ? `<h4>${pergunta}</h4>` : ''}
-                <div class="resposta-cartao">
-                    <p>${resposta}</p>
-                </div>
-            </div>
-        </div>
-    `;
+    <div class = "conteudo-cartao" onclick = "mostraReposta(this)">
+    <h1>${categoria}</h1>
+    <div class="pergunta-cartao">
+        <h4>${pergunta}</h4>
+    </div>
+    <div class="resposta-cartao">
+        <p>${resposta}</p>
+    </div>
+    
+</div>
+    `
 
-    container.appendChild(cartao);
+ container.appendChild(cartao);
+
+  
+}
+function mostraReposta (card)   {
+    const resposta = card.querySelector ('.resposta-cartao')
+if(resposta.style.display === 'none'){
+resposta.style.display = 'block';
+
+
+
+}
+    else{
+        resposta.style.display = 'none';
+
+
+    }
 }
